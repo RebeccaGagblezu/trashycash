6e1e8ac704329729d36bee8cd7500ae4bc5b2a44
RebeccaGagblezu <rebeccagagblezu@gmail.com>
2017-11-29 13:35:21 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -118,24 +118,24 @@ jQuery(document).ready(function( $ ) {
 
 
   //Google Map
-  var get_latitude = $('#google-map').data('latitude');
-  var get_longitude = $('#google-map').data('longitude');
+  
+  
 
-  function initialize_google_map() {
-    var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-    var mapOptions = {
-      zoom: 14,
-      scrollwheel: false,
-      center: myLatlng
-    };
-    var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map
-    });
-  }
-  google.maps.event.addDomListener(window, 'load', initialize_google_map);
+// bootstrap-fileinput
+// initialize with defaults
+$("#input-id").fileinput();
 
-// custom code
+// with plugin options
+// $("#input-id").fileinput({'showUpload':false, 'previewFileType':'any'});
+$("#input-id").fileinput({ uploadUrl: '/file-upload-batch/2',
+        maxFilePreviewSize: 10240});
+
+
+// $(document).on('ready', function() {
+//     $("#input-44").fileinput({
+//         uploadUrl: '/file-upload-batch/2',
+//         maxFilePreviewSize: 10240
+//     });
+// });
 
 });
